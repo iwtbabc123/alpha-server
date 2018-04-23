45f07a3221c04a8f4c04fcbde80bad00c56588c3
majianfei <majianfei1023@gmail.com>
2018-04-23 13:32:17 +0800

    use shared_ptr on class Channel

--- common/channel.h
+++ common/channel.h
@@ -1,3 +1,6 @@
+/*
+*封装sockfd
+*/
 #ifndef __ALPHA_CHANNEL_H__
 #define __ALPHA_CHANNEL_H__
 
@@ -8,33 +11,33 @@
 namespace alpha{
 
 class Channel{
-	public:
-		Channel(int fd, struct ev_io* io_watcher);
-		virtual ~Channel();
+public:
+	Channel(int fd, struct ev_io* io_watcher);
+	virtual ~Channel();
 
-		int Fd(){return fd_;}
+	int Fd(){return fd_;}
 
-		struct ev_io* GetIoWatcher(){return io_watcher_;}
-		void SetIoWatcher(struct ev_io* io_watcher){io_watcher_ = io_watcher;}
+	struct ev_io* GetIoWatcher(){return io_watcher_;}
+	void SetIoWatcher(struct ev_io* io_watcher){io_watcher_ = io_watcher;}
 
-		bool empty(){return mq_.empty();}
-		bool push_back(message_queue* mq){mq_.push_back(mq);}
-		struct message_queue* pop_front(){
-			if (mq_.empty()){
-				return nullptr;
-			}
-			struct message_queue* queue = mq_.front();
-			mq_.pop_front();
-			return queue;
+	bool empty(){return mq_.empty();}
+	bool push_back(message_queue* mq){mq_.push_back(mq);}
+	struct message_queue* pop_front(){
+		if (mq_.empty()){
+			return nullptr;
 		}
+		struct message_queue* queue = mq_.front();
+		mq_.pop_front();
+		return queue;
+	}
 
-	protected:
-		int fd_;
-		//int fd_type_;
-	private:
-		struct ev_io* io_watcher_;
+protected:
+	int fd_;
+	//int fd_type_;
+private:
+	struct ev_io* io_watcher_;
 
-		std::list<message_queue*> mq_;
+	std::list<message_queue*> mq_;
 };
 
 }
